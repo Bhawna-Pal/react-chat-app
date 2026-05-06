6cb480ca90d91943c60ea965a931bd6580a1ac73
Bhawna-Pal <bhawnapal2104@gmail.com>
2026-05-06 23:24:46 +0530

    server check

--- server/server.js
+++ server/server.js
@@ -6,16 +6,21 @@ import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
-import { AsyncLocalStorage } from 'async_hooks';
+
 
 //  Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "*";
+
 // Initialize Socket.IO server
 export const io = new Server(server, {
-    cors: {origin: "*"}
-})
+    cors: {origin: FRONTEND_URL,
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 // Store online users
 export const userSocketMap = {}; // { userId: socketId }
@@ -25,7 +30,7 @@ io.on("connection", (socket)=>{
    const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
     
-   if(userId) userSocketMap[userId] = socket.id
+   if(userId) userSocketMap[userId] = socket.id;
 
 //    Emit online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
@@ -39,11 +44,14 @@ io.on("connection", (socket)=>{
 
 // Middleware
 app.use(express.json({limit: '4mb'}));
-app.use(cors());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}));
 
 
 // Routes setup
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.get("/api/status", (req, res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
